5c0c8e3a9d9da5b9bfce834030f5f4ff718edce4
inxole <inxole@gmail.com>
2024-02-26 01:25:01 +0900

    add files and change files

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
 FROM mcr.microsoft.com/devcontainers/typescript-node:20-bookworm
+EXPOSE 5173
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css'
+import Model from './Model/Test_page' 
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from "react"
 
 const App = () => {
   return (
     <span>
-      
+      <Suspense fallback={null}>	
+        <Canvas>
+          <Model/> 
+        </Canvas>
+      </Suspense>
     </span>
   )
 }
