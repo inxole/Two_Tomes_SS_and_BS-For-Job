2f85be3dde6ca8c58ad467a2448a62b71526e740
inxole <inxole@gmail.com>
2024-05-25 15:34:29 +0900

    create back page and attached to skeleton

--- frontend_Twotomes/src/Page.tsx
+++ frontend_Twotomes/src/Page.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useReducer } from 'react'
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, StandardMaterial, Color3, AxesViewer, Skeleton, Bone, Matrix, VertexData, DefaultRenderingPipeline, PointerInfo, PointerEventTypes, MeshBuilder } from '@babylonjs/core'
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, StandardMaterial, Color3, AxesViewer, Skeleton, Bone, Matrix, VertexData, DefaultRenderingPipeline, PointerInfo, PointerEventTypes, MeshBuilder, DynamicTexture } from '@babylonjs/core'
 import { Mesh } from '@babylonjs/core/Meshes/mesh'
 import { SkeletonViewer } from '@babylonjs/core/Debug/skeletonViewer'
 import { Inspector } from '@babylonjs/inspector'
@@ -17,7 +17,6 @@ function animationReducer(state: boolean, action: Action) {
             }
             return !state
         default:
-            console.log("error")
             throw new Error()
     }
 }
@@ -28,15 +27,11 @@ function toggleAnimation(pointerInfo: PointerInfo, dispatch: React.Dispatch<Acti
             dispatch({
                 type: "TOGGLE",
                 open: () => {
-                    console.log("open")
                     scene.beginAnimation(skeleton, 0, 60, true, undefined, () => {
-                        console.log("opened")
                     })
                 },
                 close: () => {
-                    console.log("close")
                     scene.beginAnimation(skeleton, 60, 120, true, undefined, () => {
-                        console.log("closed")
                     })
                 }
             })
@@ -121,16 +116,98 @@ const BabylonScene = () => {
         vertexData.matricesIndices = influences
         vertexData.applyToMesh(page)
 
+        const FrontTexture = new DynamicTexture("DynamicTexture", 512, scene, true)
+        FrontTexture.hasAlpha = true
+        const font = "bold 44px monospace"
+        FrontTexture.drawText("Hello, Babylon.js!", null, null, font, "#000000", "#ffffff", true)
+
         const pageMaterial = new StandardMaterial("pageMat", scene)
+        pageMaterial.diffuseTexture = FrontTexture
         pageMaterial.diffuseColor = new Color3(1, 1, 1)
         pageMaterial.backFaceCulling = false
         page.material = pageMaterial
         page.rotation = new Vector3(Math.PI / 2, 0, 0)
+
+        const back_page = new Mesh("back_page", scene)
+        const back_positions = []
+        const back_indices = []
+        const back_normals = []
+        const back_uvs = []
+        const back_weights = []
+        const back_influences = []
+
+        for (let i = 0; i <= heightSubdivisions; i++) {
+            for (let j = 0; j <= widthSubdivisions; j++) {
+                const x = (j * width) / widthSubdivisions - width / 2
+                const y = (i * height) / heightSubdivisions - height / 2
+                back_positions.push(x, y, 0.0001)
+                back_normals.push(0, 0, 1)
+                back_uvs.push(j / widthSubdivisions, i / heightSubdivisions)
+
+                // ウェイトとインフルエンスの設定
+                const weight = j / widthSubdivisions
+                back_weights.push(weight, 1 - weight, 0, 0)
+                const influence1 = Math.min(j, widthSubdivisions - 1)
+                const influence2 = Math.min(j + 1, widthSubdivisions)
+                back_influences.push(influence1, influence2, 0, 0)
+            }
+        }
+
+        for (let i = 0; i < heightSubdivisions; i++) {
+            for (let j = 0; j < widthSubdivisions; j++) {
+                const a = i * (widthSubdivisions + 1) + j
+                const b = a + 1
+                const c = a + (widthSubdivisions + 1)
+                const d = c + 1
+                back_indices.push(a, b, d)
+                back_indices.push(a, d, c)
+            }
+        }
+
+        const back_vertexData = new VertexData()
+        back_vertexData.positions = back_positions
+        back_vertexData.indices = back_indices
+        back_vertexData.normals = back_normals
+        back_vertexData.uvs = back_uvs
+        back_vertexData.matricesWeights = back_weights
+        back_vertexData.matricesIndices = back_influences
+        back_vertexData.applyToMesh(back_page)
+
+        const BackTexture = new DynamicTexture("DynamicTexture", { width: 345, height: 512 }, scene, true)
+        BackTexture.hasAlpha = true
+        const back = "bold 44px monospace"
+
+        // Canvasの2Dコンテキストにアクセス
+        const ctx = BackTexture.getContext()
+
+        // 背景を白に設定
+        ctx.fillStyle = "#ffffff"
+        ctx.fillRect(0, 0, BackTexture.getSize().width, BackTexture.getSize().height)
+
+        // テキストを描画する前にコンテキストを傾ける
+        ctx.save()
+        ctx.translate(270, 245)
+        ctx.rotate(Math.PI / 1)// 45度傾ける
+        ctx.fillStyle = "#000000"
+        ctx.font = back
+        ctx.fillText("I'm back!", 0, 0)
+        ctx.restore()
+
+        // テクスチャを更新
+        BackTexture.update(false)
+
+        const back_pageMaterial = new StandardMaterial("pageMat", scene)
+        back_pageMaterial.diffuseTexture = BackTexture
+        back_pageMaterial.diffuseColor = new Color3(1, 1, 1)
+        back_pageMaterial.backFaceCulling = false
+        back_page.material = back_pageMaterial
+        back_page.rotation = new Vector3(Math.PI / 2, 0, 0)
+
         const skeleton = new Skeleton("skeleton", "001", scene)
 
         function createHitBoxMaterial(boneName: string, scene: Scene): StandardMaterial {
             const material = new StandardMaterial(`hitBoxMat_${boneName}`, scene)
-            material.alpha = 0.3
+            material.alpha = 0.0
             material.diffuseColor = new Color3(0.5, 0.5, 1)
             return material
         }
@@ -153,6 +230,7 @@ const BabylonScene = () => {
         }
 
         page.skeleton = skeleton
+        back_page.skeleton = skeleton
         const skeletonViewer = new SkeletonViewer(skeleton, page, scene, false, 3, {
             displayMode: SkeletonViewer.DISPLAY_SPHERE_AND_SPURS
         })
