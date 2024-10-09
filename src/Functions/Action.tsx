695a00b86b2873a900cf87c399baf58492f06d56
inxole <inxole@gmail.com>
2024-10-09 15:48:00 +0900

    Refactor openPageAnimation and closePageAnimation functions

--- src/Functions/Action.tsx
+++ src/Functions/Action.tsx
@@ -73,46 +73,62 @@ export function useDynamicReducers(reducer: Reducer<PageState, Action>, initialS
 
 /**
  * Open page animation
- * @param animationData animation group reference
+ * @param sortedAnimationData animation group reference
  * @returns void
  */
 export function openPageAnimation(animationData: AnimationGroup[]) {
     const AD = AnimationDictionary
-    // Open/switch cover
-    animationData[AD.N_0_90_Group].start(true), animationData[AD.R_90_0_Group].stop()
-    animationData[AD.Action_0_90_BS].start(true), animationData[AD.Non_Action_BS].stop()
+    const sortedAnimationData = [...animationData].sort((a, b) => a.name.localeCompare(b.name))
 
-    setTimeout(() => { animationData[AD.Action_0_90_BS].stop() }, 1000) // Repeat prevention
+    // Open/switch cover
+    sortedAnimationData[AD.F_0_90_Group].start(true), sortedAnimationData[AD.R_90_0_Group].stop()
+    sortedAnimationData[AD.BS_Action_0_90].start(true), sortedAnimationData[AD.BS_Non_Action].stop()
+    sortedAnimationData[AD.SS_Action_0_90].start(true), sortedAnimationData[AD.SS_Non_Action].stop()
+
+    setTimeout(() => { sortedAnimationData[AD.BS_Action_0_90].stop() }, 1000) // Tome_BS Repeat prevention
+    setTimeout(() => { sortedAnimationData[AD.BS_Stay_90].start() }, 1000) // Tome_BS Start holding
+    setTimeout(() => { sortedAnimationData[AD.SS_Action_0_90].stop() }, 1000) // Tome_SS Repeat prevention
+    setTimeout(() => { sortedAnimationData[AD.SS_Stay_90].start() }, 1000) // Tome_SS Start holding
 
     // Move pages all at once and switch
-    animationData[AD.N_Controller].start(true), animationData[AD.R_Controller].stop()
-    animationData[AD.N_Animation_Group].start(true), animationData[AD.R_Animation_Group].stop()
+    sortedAnimationData[AD.F_Controller].start(true), sortedAnimationData[AD.R_Controller].stop()
+    sortedAnimationData[AD.F_Animation_Group].start(true), sortedAnimationData[AD.R_Animation_Group].stop()
 }
 
 export function closePageAnimation(animationData: AnimationGroup[]) {
     const AD = AnimationDictionary
-    // Close/switch cover
-    animationData[AD.R_90_0_Group].start(true), animationData[AD.N_0_90_Group].stop()
-    animationData[AD.Action_back_BS].start(true)
+    const sortedAnimationData = [...animationData].sort((a, b) => a.name.localeCompare(b.name))
 
-    setTimeout(() => { animationData[AD.Action_back_BS]?.stop() }, 1000) // Repeat prevention
+    // Close/switch cover
+    sortedAnimationData[AD.R_90_0_Group].start(true), sortedAnimationData[AD.F_0_90_Group].stop()
+    sortedAnimationData[AD.BS_Action_back].start(true), sortedAnimationData[AD.BS_Action_0_90].stop(true)
+    sortedAnimationData[AD.SS_Action_back].start(true), sortedAnimationData[AD.SS_Action_0_90].stop(true)
+
+    setTimeout(() => { sortedAnimationData[AD.BS_Action_back]?.stop() }, 1000) // Tome_BS Repeat prevention
+    sortedAnimationData[AD.BS_Stay_90].stop() // Tome_BS Stop holding
+    setTimeout(() => { sortedAnimationData[AD.SS_Action_back]?.stop() }, 1000) // Tome_SS Repeat prevention
+    sortedAnimationData[AD.SS_Stay_90].stop() // Tome_SS Stop holding
 
     // Move pages all at once and switch
-    animationData[AD.R_Controller].start(true), animationData[AD.N_Controller].stop()
-    animationData[AD.R_Animation_Group].start(true), animationData[AD.N_Animation_Group].stop()
+    sortedAnimationData[AD.R_Controller].start(true), sortedAnimationData[AD.F_Controller].stop()
+    sortedAnimationData[AD.R_Animation_Group].start(true), sortedAnimationData[AD.F_Animation_Group].stop()
 }
 
 export enum AnimationDictionary {
-    N_Controller = 0,
-    R_Controller = 1,
-    N_Animation_Group = 2,
-    R_Animation_Group = 3,
-    N_0_90_Group = 4,
-    R_90_0_Group = 5,
-    Action_0_90_BS = 6,
-    Stay_90_BS = 7,
-    Action_back_BS = 8,
-    Non_Action_BS = 9,
+    BS_Non_Action = 0,
+    BS_Action_0_90 = 1,
+    BS_Stay_90 = 2,
+    BS_Action_back = 3,
+    SS_Non_Action = 4,
+    SS_Action_0_90 = 5,
+    SS_Stay_90 = 6,
+    SS_Action_back = 7,
+    F_Controller = 8,
+    F_Animation_Group = 9,
+    F_0_90_Group = 10,
+    R_Controller = 11,
+    R_Animation_Group = 12,
+    R_90_0_Group = 13,
 }
 
 export function pageFrontAnimation(scene: Scene, index: number) {
@@ -128,13 +144,17 @@ export function pageBackAnimation(scene: Scene, index: number) {
 }
 
 //glb_animationの内訳
-// 0: Object { name: "N_Controller", _from: 1000, _to: 1060, … }
-// 1: Object { name: "R_Controller", _from: 1060, _to: 1120, … }
-// 2: Object { name: "N_Animation_Group", _from: 1000, _to: 1050, … }
-// 3: Object { name: "R_Animation_Group", _from: 1050, _to: 1100, … }
-// 4: Object { name: "N_0_90_Group", _from: 0, _to: 50, … }
-// 5: Object { name: "R_90_0_Group", _from: 0, _to: 50, … }
-// 6: Object { name: "000_BS_Non_Action", _from: 0, _to: 600, … } Do not use as it is the initial value
-// 7: Object { name: "001_BS_action_0_90", _from: 0, _to: 600, … }
-// 8: Object { name: "002_BS_stay_90", _from: 0, _to: 600, … }
-// 9: Object { name: "003_BS_action_back", _from: 0, _to: 600, … }
+// 0: Object { name: "000_BS_Non_Action", _from: 0, _to: 600, … }
+// 1: Object { name: "001_BS_action_0_90", _from: 0, _to: 600, … }
+// 2: Object { name: "002_BS_stay_90", _from: 0, _to: 600, … }
+// 3: Object { name: "003_BS_action_back", _from: 0, _to: 600, … }
+// 4: Object { name: "004_SS_Non_Action", _from: 0, _to: 600, … }
+// 5: Object { name: "005_SS_action_0_90", _from: 0, _to: 600, … }
+// 6: Object { name: "006_SS_stay_90", _from: 0, _to: 600, … }
+// 7: Object { name: "007_SS_action_back", _from: 0, _to: 600, … }
+// 8: Object { name: "F_0_90_Group", _from: 0, _to: 50, … }
+// 9: Object { name: "F_Animation_Group", _from: 1000, _to: 1050, … }
+// 10: Object { name: "F_Controller", _from: 1000, _to: 1060, … }
+// 11: Object { name: "R_90_0_Group", _from: 0, _to: 50, … }
+// 12: Object { name: "R_Animation_Group", _from: 1050, _to: 1100, … }​
+// 13: Object { name: "R_Controller", _from: 1060, _to: 1120, … }
