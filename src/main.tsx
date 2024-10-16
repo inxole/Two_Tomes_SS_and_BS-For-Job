3a30e801b67a4eabed1c772d14218b7b79df295c
inxole <inxole@gmail.com>
2024-10-16 09:29:23 +0000

    Add NieR-Regular font to the project

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
 import App from './App.tsx'
+import '/app/src/assets/fonts/NieR-Regular.ttf'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
