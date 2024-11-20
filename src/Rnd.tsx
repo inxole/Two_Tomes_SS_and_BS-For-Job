0db3ff408e7c1e150ccc7b70a4f8352225a3c2e6
inxole <inxole@gmail.com>
2024-11-20 11:10:17 +0000

    Refactor RndComponent and User_interface to enhance layout structure and add icon functionality

--- src/Rnd.tsx
+++ src/Rnd.tsx
@@ -3,10 +3,15 @@ import { Rnd } from 'react-rnd'
 import TextInput from './Blocks/TextBlock'
 import FontSizeSlider from './Blocks/FontSizeBlock'
 import { CoverState, PageSlider } from './Blocks/SlideBlock'
+import { grey } from '@mui/material/colors'
+import { Stack } from '@mui/material'
+import CenterFocusStrongTwoToneIcon from '@mui/icons-material/CenterFocusStrongTwoTone';
+import ArchiveTwoToneIcon from '@mui/icons-material/ArchiveTwoTone'
 
 function RndComponent() {
   const [, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight })
   const [position, setPosition] = useState({ x: (window.innerWidth - 350) / 2, y: 10 })
+  const sub_position = 480
 
   useEffect(() => {
     const handleResize = () => {
@@ -21,30 +26,42 @@ function RndComponent() {
   }, [])
 
   return (
-    <Rnd
-      default={{ x: position.x, y: position.y, width: 350, height: 480 }}
-      style={{ backgroundColor: 'rgba(255, 255, 255, 0.65)', borderRadius: '8px', paddingLeft: '4px', paddingBottom: '4px', paddingTop: '40px' }}
-      enableResizing={false}
-      disableDragging={true}
-      position={{ x: position.x, y: position.y }}
-    >
-      <div
-        style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}
-        onMouseDown={(e) => e.stopPropagation()}
-        onTouchStart={(e) => e.stopPropagation()}
+    <>
+      <Rnd
+        default={{ x: position.x, y: position.y, width: 350, height: 480 }}
+        style={{ backgroundColor: 'rgba(255, 255, 255, 0.65)', borderRadius: '8px', paddingLeft: '4px', paddingBottom: '4px', paddingTop: '40px' }}
+        enableResizing={false}
+        disableDragging={true}
+        position={{ x: position.x, y: position.y }}
       >
-        <div style={{ width: '330px', height: '200px', display: 'flex', ...BorderStyle }}>
-          <TextInput />
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          <div style={{ width: '330px', height: '200px', display: 'flex', ...BorderStyle }}>
+            <TextInput />
+          </div>
+          <div style={{ width: '330px', height: '100px', display: 'flex', ...BorderStyle }}>
+            <FontSizeSlider />
+          </div>
+          <div style={{ width: '330px', height: '100px', display: 'flex', ...BorderStyle }}>
+            <CoverState />
+            <PageSlider />
+          </div>
         </div>
-        <div style={{ width: '330px', height: '100px', display: 'flex', ...BorderStyle }}>
-          <FontSizeSlider />
+      </Rnd>
+      <Rnd
+        default={{ x: position.x, y: sub_position, width: 350, height: 40 }}
+        position={{ x: position.x, y: sub_position }}
+        enableResizing={false}
+        disableDragging={true}
+        style={{ pointerEvents: 'none' }}
+      >
+        <div style={{ width: '350px', margin: '0 auto', display: 'flex', justifyContent: 'center' }}>
+          <Stack direction="row" alignItems="center" justifyContent="center" spacing={2} sx={{ pointerEvents: 'auto' }}>
+            <ArchiveTwoToneIcon fontSize='large' style={{ paddingTop: '10px', color: grey[100], cursor: 'pointer' }} />
+            <CenterFocusStrongTwoToneIcon fontSize='large' style={{ paddingTop: '10px', color: grey[100], cursor: 'pointer' }} />
+          </Stack>
         </div>
-        <div style={{ width: '330px', height: '100px', display: 'flex', ...BorderStyle }}>
-          <CoverState />
-          <PageSlider />
-        </div>
-      </div>
-    </Rnd>
+      </Rnd>
+    </>
   )
 }
 
--- src/UI_Rnd.tsx
+++ src/UI_Rnd.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react"
 import { Rnd } from "react-rnd"
+import { Stack } from '@mui/material'
+import { amber, grey } from '@mui/material/colors'
+import BookTwoToneIcon from '@mui/icons-material/BookTwoTone'
+import ArchiveTwoToneIcon from '@mui/icons-material/ArchiveTwoTone'
 
 function User_interface() {
     const [, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight })
     const [position, setPosition] = useState({ x: (window.innerWidth - 350) / 2, y: window.innerHeight - 480 - 10 })
+    const sub_position = window.innerHeight - 480 - 50
 
     useEffect(() => {
         const handleResize = () => {
@@ -18,14 +23,31 @@ function User_interface() {
     }, [])
 
     return (
-        <Rnd
-            default={{ x: position.x, y: position.y, width: 350, height: 480 }}
-            style={{ backgroundColor: 'rgba(255, 255, 255, 0.65)', borderRadius: '8px', paddingLeft: '4px', paddingBottom: '4px', paddingTop: '40px' }}
-            enableResizing={false}
-            disableDragging={true}
-            position={{ x: position.x, y: position.y }} 
-        >
-        </Rnd>
+        <>
+            <Rnd
+                default={{ x: position.x, y: sub_position, width: 350, height: 40 }}
+                position={{ x: position.x, y: sub_position }}
+                enableResizing={false}
+                disableDragging={true}
+                style={{ pointerEvents: 'none' }}
+            >
+                <div style={{ width: '200px', margin: '0 auto', display: 'flex', justifyContent: 'center' }}>
+                    <Stack direction="row" alignItems="center" justifyContent="center" spacing={2} sx={{ pointerEvents: 'auto' }}>
+                        <BookTwoToneIcon fontSize="large" style={{ color: amber[100], cursor: 'pointer' }} />
+                        <ArchiveTwoToneIcon fontSize="large" style={{ color: grey[100], cursor: 'pointer' }} />
+                        <BookTwoToneIcon fontSize="large" style={{ color: grey[800], cursor: 'pointer' }} />
+                    </Stack>
+                </div>
+            </Rnd>
+            <Rnd
+                default={{ x: position.x, y: position.y, width: 350, height: 480 }}
+                style={{ backgroundColor: 'rgba(255, 255, 255, 0.65)', borderRadius: '8px', paddingLeft: '4px', paddingBottom: '4px', paddingTop: '4px' }}
+                enableResizing={false}
+                disableDragging={true}
+                position={{ x: position.x, y: position.y }}
+            >
+            </Rnd>
+        </>
     )
 }
 
